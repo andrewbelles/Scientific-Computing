a8d8400d22c512fd2fda5d492d3b0c4f96f7ce8a
andrewbelles <andrewbelles31@gmail.com>
2025-02-22 16:02:56 -0500

    Better Python Plotting Utility

--- regression_complex/visualize.py
+++ regression_complex/visualize.py
@@ -1,66 +1,116 @@
 import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D 
+from scipy.interpolate import griddata
 import numpy as np 
+
 import argparse 
+import glob
+import os
 
-def process_1d_data(path):
-
+# Takes in a path to data and the number of expected inputs/outputs
+def process_data(path, expected_io_count=None):
     data = []
+    with open(path, "r", encoding='utf-8') as f:
+        for line in f:
+            elements = list(map(float, line.strip().split(',')))
+            if expected_io_count != len(elements):
+                raise ValueError(f"Expected {expected_io_count} values, got {len(elements)}")
+            data.append(elements)
 
-    with open(path, "r", encoding='utf-8') as file_object:
-        for line in file_object:
-            x = float(line.strip())
-            data.append(x)
     return np.array(data)
 
-def process_2d_data(path):
 
-    data = []
+def plot_data(inputs, outputs, title="Data Plot", input_dim=0, output_dim=0): 
 
-    with open(path, "r", encoding='utf-8') as file_object:
-        for line in file_object:
+    fig = plt.figure(figsize=(10,6))
 
-            x, y = map(float, line.split(','))
-            data.append([x, y])
-    return np.array(data)
+    # Reshape to column if 1d 
+    if inputs.ndim == 1:
+        inputs = inputs.reshape(-1, 1)
+    if outputs.ndim == 1:
+        outputs = outputs.reshape(-1, 1)
 
-def plot_test_data(x, y, title="Data Plot"): 
 
-    print(title)
+    if input_dim + output_dim == 2:
 
-    plt.scatter(x, y)
-    plt.xlabel("Time [0:10]")
-    plt.ylabel("Amplitude")
+        x_label = f'Input Dimension {input_dim}' if inputs.shape[1] > 1 else 'Input'
+        y_label = f'Output Dimension {output_dim}' if outputs.shape[1] > 1 else 'Output'
+
+        plt.scatter(inputs[:, input_dim-1], outputs[:, output_dim-1], alpha=0.5)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+    
+    elif input_dim + output_dim == 3:
+
+
+        if inputs.ndim == 1:
+            x = inputs[:, 0]
+            y = outputs[:, 1]
+            z = outputs[:, 2]
+        elif inputs.ndim == 2:
+            x = inputs[:, 0]
+            y = inputs[:, 1]
+            z = outputs[:, 0]
+
+
+        xi = np.linspace(x.min(), x.max(), 100)
+        yi = np.linspace(y.min(), y.max(), 100)
+        X, Y = np.meshgrid(xi, yi)
+
+        try:
+            Z = griddata((x, y), z, (X, Y), method='linear')
+
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot_surface(X, Y, Z, cmap='viridis')
+        except Exception as e:
+            print(f"Error {e}")
+            plt.close(fig)
+            return
+
     plt.title(title)
-    plt.savefig(title + ".png")
-    plt.close()
+    plt.grid(True)
+
+    tag = f"images/{title}.png"
+    plt.savefig(tag, bbox_inches='tight')
+    plt.close(fig)
+    print(tag)
 
 def main():
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("base_path")
     parser.add_argument("expected")
-    parser.add_argument("start", type=int)
-    parser.add_argument("end", type=int)
+    parser.add_argument("--dims", nargs=2, type=int, default=[1,1])
+    parser.add_argument("--range", nargs=2, type=int, default=[-1,-1])
     args = parser.parse_args()
 
-    input_path = args.expected 
-    base_epoch_data = "output_files/" + args.base_path
-    png_path = "images/" + args.base_path
+    input_dims  = args.dims[0]
+    output_dims = args.dims[1]
+    expected_io = input_dims + output_dims
 
-    # Vec is the input and expected output
-    vec    = process_2d_data(input_path)
+    # Clear image folder 
+    pattern = os.path.join("images/", "*.png")
+    to_delete = glob.glob(pattern)
 
-    # Grab inputs 
-    inputs   = vec[:, 0]
-    expected = vec[:, 1]
+    for file in to_delete:
+        os.remove(file)
+        print(f"Deleted {file}")
 
-    for epoch in range(args.start, args.end+1):
-        epoch_path = f"{base_epoch_data}{epoch:04d}.txt"
-        
-        title = f"{png_path}{epoch:04d}"
-        output = process_1d_data(epoch_path)
-        plot_test_data(inputs, output, title=title)
+    # Load data expected from command line inputs 
+    loaded_data = process_data(args.expected, expected_io)
+    inputs      = loaded_data[:, :input_dims]
+    expected    = loaded_data[:, input_dims:]
+    print("Processed Data")
 
-    plot_test_data(inputs, expected, "images/expected_output")
+    plot_data(inputs, expected, "expected_output", input_dims, output_dims)
+
+    # Only plot epoch data if expected 
+    if args.range[0] != -1:
+        for epoch in range(args.range[0], args.range[1]+1):
+            epoch_path = f"outputs/epoch_{epoch:04d}.txt"
+            
+            network_outputs = process_data(epoch_path, output_dims)
+
+            plot_data(inputs, network_outputs, f"epoch_{epoch:04d}", input_dims, output_dims)
 
 if __name__ == "__main__":
     main()
