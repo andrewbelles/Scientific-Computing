0e43fef9433c7ada01dbba284023614f5377a145
andrewbelles <andrewbelles31@gmail.com>
2025-02-23 21:13:13 -0500

    Prediction and Learn Mode + Sine Wave Approximation

--- regression_complex/create_data.py
+++ regression_complex/create_data.py
@@ -1,27 +1,24 @@
 import argparse 
-import random
-import sys 
 import numpy as np
 
-
-def create_1D_test_data(size):
+def create_1D_test_data(size, lower, upper):
     x = []
-    for i in range(size):
-        new = np.random.uniform(0.0, 10.0)
+    for _ in range(size):
+        new = np.random.uniform(lower, upper)
         x.append(new)
 
     x = np.array(x)
-    y = 2*x-2
+    y = np.sin(x)
 
     return np.stack((x, y)) 
 
 
-def create_2D_test_data(size):
+def create_2D_test_data(size, lower, upper):
     x = []
     y = []
-    for i in range(size):
-        _x = np.random.uniform(0.0, 10.0)
-        _y = np.random.uniform(0.0, 10.0)
+    for _ in range(size):
+        _x = np.random.uniform(lower, upper)
+        _y = np.random.uniform(lower, upper)
 
         x.append(_x)
         y.append(_y)
@@ -50,11 +47,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename")
     parser.add_argument("size", type=int)
+    parser.add_argument("--range", nargs=2, type=float, default=[0.0,10.0])
 
     args = parser.parse_args()
 
-    data = create_2D_test_data(args.size)
+    lower = args.range[0]
+    upper = args.range[1]
+
+    data = create_2D_test_data(args.size, lower, upper)
     write_2D_data(args.filename, data)
+
 
 if __name__ == "__main__":
     main()
